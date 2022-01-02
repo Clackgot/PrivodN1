22d8acb0182c8fae7f63cbd6284ea007cf11ac83
clackgot <clackgot@gmail.com>
2022-01-02 10:37:17 +0300

    Исправлен метод PostCharge

---
+++ Sanya2/Sanya2.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+int PostCharge(int num, int base, int k = 0)
+{
+	if (num == 0)
+		return k;
+	k += num % base + 10 * PostCharge(num / base, base, k);
+	return k;
+}
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	int a, b;
+	cout << "Введите десятичное число: " << endl;
+	cin >> a;
+	cout << "Введите систему счисления вывода: " << endl;
+	cin >> b;
+	cout << "ВЫВОД:" << PostCharge(a, b);
+}
